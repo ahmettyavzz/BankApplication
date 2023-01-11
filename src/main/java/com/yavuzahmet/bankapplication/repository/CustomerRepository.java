ccab0d998d5e747b01e4e7a7a8aeededfbc4925c
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2023-01-12 01:13:25 +0300

    create CustomerRepository to repository layer

---
+++ src/main/java/com/yavuzahmet/bankapplication/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.yavuzahmet.bankapplication.repository;
+
+import com.yavuzahmet.bankapplication.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, String> {
+}
