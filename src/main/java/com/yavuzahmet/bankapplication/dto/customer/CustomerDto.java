75d3524506bb59e3dbe1cd5b02fa47175de554a1
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2023-01-18 22:52:18 +0300

    create CustomerDto in dto layer

--- src/main/java/com/yavuzahmet/bankapplication/dto/customer/CustomerDto.java
+++ src/main/java/com/yavuzahmet/bankapplication/dto/customer/CustomerDto.java
@@ -1,14 +1,19 @@
-package com.yavuzahmet.bankapplication.dto;
+package com.yavuzahmet.bankapplication.dto.customer;
 
+import com.yavuzahmet.bankapplication.dto.CityDto;
+import com.yavuzahmet.bankapplication.model.Account;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CustomerDto {
     private String id;
+    private List<Account> accounts;
     private String name;
     private Integer dateOfBirth;
     private CityDto city;
