0a9fe8dd29afbd4f3bd9dafbee11ec239a1dc1a5
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2023-01-18 22:47:50 +0300

    create Currency in model layer

---
+++ src/main/java/com/yavuzahmet/bankapplication/controller/AccountController.java
@@ -0,0 +1,55 @@
+package com.yavuzahmet.bankapplication.controller;
+
+import com.yavuzahmet.bankapplication.converter.AccountConverter;
+import com.yavuzahmet.bankapplication.dto.account.AccountDto;
+import com.yavuzahmet.bankapplication.dto.account.CreateAccountRequest;
+import com.yavuzahmet.bankapplication.dto.account.UpdateAccountRequest;
+import com.yavuzahmet.bankapplication.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/account")
+@RequiredArgsConstructor
+public class AccountController {
+
+    private final AccountService accountService;
+    private final AccountConverter accountConverter;
+
+    @GetMapping
+    public ResponseEntity<List<AccountDto>> getAllAccount() {
+        return ResponseEntity.ok(accountService.getAccounts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountDto> getAccountById(@PathVariable String id) {
+        return ResponseEntity.ok(accountService.getAccountById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAccountById(@PathVariable String id) {
+        accountService.deleteAccount(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<AccountDto> createAccount(@RequestBody CreateAccountRequest request) {
+        return ResponseEntity.ok(accountService.createAccount(accountConverter.createAccountRequestToAccount(request)));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AccountDto> updateAccount(@PathVariable String id, @RequestBody UpdateAccountRequest request) {
+        return ResponseEntity.ok(accountService.updateAccount(id, accountConverter.updateAccountRequestToAccount(request)));
+    }
+
+}
