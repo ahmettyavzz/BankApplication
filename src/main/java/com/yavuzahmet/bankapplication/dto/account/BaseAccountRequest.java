6766f66d6d809db8d2f83baa882ed4d712b59129
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2023-01-26 17:27:26 +0300

    add valid to BaseAccountRequest

--- src/main/java/com/yavuzahmet/bankapplication/dto/account/BaseAccountRequest.java
+++ src/main/java/com/yavuzahmet/bankapplication/dto/account/BaseAccountRequest.java
@@ -2,6 +2,8 @@ package com.yavuzahmet.bankapplication.dto.account;
 
 import com.yavuzahmet.bankapplication.dto.CityDto;
 import com.yavuzahmet.bankapplication.model.Currency;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +12,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BaseAccountRequest {
+
+    @NotNull(message = "CustomerId can not null!")
     private String customerId;
+
+    @NotNull(message = "Balance can not null!")
+    @Min(0)
     private Double balance;
+
+    @NotNull(message = "City can not null!")
     private CityDto city;
+
+    @NotNull(message = "Currency can not null!")
     private Currency currency;
 }
